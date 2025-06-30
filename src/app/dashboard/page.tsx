1d236e76c1d129dc1058e7be60b2ff672609415c
Kaitlyn <kjoikle@gmail.com>
2025-06-30 19:20:36 -0400

    navbar changed to boilerplate

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,7 @@
-import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import PlaylistsDisplay from "@/components/Dashboard/PlaylistsDisplay";
 
 const Dashboard = async () => {
-  const supabase = await createClient();
-
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
-    redirect("/login");
-  }
-
   return (
     <div>
       <h1 className="text-4xl font-bold text-center mt-10">
